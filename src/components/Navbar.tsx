031729876574567eb8d6c2f5cfbcc5288ea9e8a8
Soham Sarkar <soham.ss1@gmail.com>
2021-07-17 14:07:00 +0530

    navbar complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
-import { Flex, Heading, Icon, Text } from '@chakra-ui/react';
+import { Container, Flex, Heading, Icon, Text } from '@chakra-ui/react';
 import React from 'react';
 import { HiOutlineMoon } from 'react-icons/hi';
 
 export default function Navbar() {
   return (
-    <Flex justify='space-between'>
-      <Heading as="h1">Where in the world?</Heading>
-      <Flex>
-        <Icon as={HiOutlineMoon} />
-        <Text> Dark Mode</Text>
+    <Flex justify="space-between" px="4em" boxShadow="0px 0px 5px 0px gray">
+      <Heading size="md">Where in the world?</Heading>
+      <Flex align="center">
+        <Container>
+          <Icon as={HiOutlineMoon} boxSize={20} />
+        </Container>
+        <Text ml={4}>Dark Mode</Text>
       </Flex>
     </Flex>
   );
